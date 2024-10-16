6cd3020116e02ca0d9b0560071efc38a9a6fdccd
Mohammad Azam <azamsharp@gmail.com>
2024-10-16 11:38:00 -0500

    added authentication middleware
    At present getMyProducts is protected by authentication middleware.

---
+++ hello-market-server/middlewares/authMiddleware.js
@@ -0,0 +1,37 @@
+
+const jwt = require('jsonwebtoken');
+const models = require('../models') 
+
+const authenticate = async (req, res, next) => {
+
+    // get the authorization header 
+    const authHeader = req.headers['authorization']
+
+    if(!authHeader) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    if(!token) {
+        return res.status(401).json({ message: 'Invalid token format' });
+    }
+
+    try {
+        
+        const decoded = jwt.verify(token, 'SECRETKEY');
+        const user = await models.User.findByPk(decoded.userId)
+        if(!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        req.user = user 
+        next() 
+
+    } catch (error) {
+        return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+
+}
+
+module.exports = authenticate 
--- hello-market-server/routes/product.js
+++ hello-market-server/routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router() 
 const productController = require('../controllers/productController')
-const { body } = require('express-validator');
-const { productValidator } = require('../utils/validators/validators') 
+const { productValidator } = require('../utils/validators/validators'); 
+const authenticate = require('../middlewares/authMiddleware');
+
 
 router.post('/', productValidator, productController.create)
 router.get('/', productController.getAllProducts)
-router.get('/user/:userId', productController.getMyProducts)
+router.get('/user/:userId', authenticate, productController.getMyProducts)
 router.post('/upload', productController.upload)
 
 module.exports = router 
